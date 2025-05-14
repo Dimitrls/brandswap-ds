e90f2ae52c28a162405eff8d9b0867bbeacce867
Dimitrls <you1983a@yahoo.com>
2025-05-14 11:59:33 +0300

    Selectbox component

---
+++ src/FormElements/Selectbox.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useRef, useEffect } from 'react';
+import styles from './Selectbox.module.css';
+
+/**
+ * @typedef {Object} SelectboxProps
+ * @property {string} [label] - Label above the select
+ * @property {string[]} options - Array of option strings
+ * @property {function} [onChange] - Optional change handler
+ */
+
+const Selectbox = ({ label, options = [], onChange }) => {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState(options[0] || '');
+  const ref = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
+  const handleSelect = (option) => {
+    setSelected(option);
+    setOpen(false);
+    onChange && onChange(option);
+  };
+
+  return (
+    <div className={styles.wrapper} ref={ref}>
+      {label && <label className={styles.label}>{label}</label>}
+      <div className={styles.select} onClick={() => setOpen(!open)}>
+        {selected}
+        <span className={styles.arrow}>▼</span>
+      </div>
+      {open && (
+        <ul className={styles.dropdown}>
+          {options.map((option, idx) => (
+            <li
+              key={idx}
+              className={styles.option}
+              onClick={() => handleSelect(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Selectbox; 
---
+++ src/stories/Selectbox.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Selectbox from '../FormElements/Selectbox';
+
+export default {
+  title: 'Form elements/Selectbox',
+  component: Selectbox,
+};
+
+export const Default = () => (
+  <Selectbox
+    label="Choose an option"
+    options={['Option 1', 'Option 2', 'Option 3']}
+    onChange={option => alert(`Selected: ${option}`)}
+  />
+); 
